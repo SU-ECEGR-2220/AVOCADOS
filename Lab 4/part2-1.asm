ec42710d06390afe79a81d094fe1ce53a616565a
Thanh Nguyen <nguye408@seattleu.edu>
2018-05-13 12:36:50 -0700

    changes

--- Lab 4/part2-1.asm
+++ Lab 4/part2-1.asm
@@ -19,26 +19,27 @@ main:
 	
 	jal converter		#call function converter
 	
-	#C
+	#Ctemp
+	
+	
+	fmv.s   fa1,ft6    # save f10 to Val2
+	fmv.s   fa0,fa1
 	li	a7,4			#system call for print string
 	la	a0,ctemp
 	ecall
-	
-	fmv.s   fa1,ft6    # save f10 to Val2
-	#fmv.x.s	t1, ft6		# copy FP value to t reg
-	#fsw	fa1, 0(a0)	#store FP val to memory
 	li	a7,2			#system call for printing float fa0 in ascii
 	ecall
 	
-	#K
+	#Ktemp
+	
+	fmv.s   fa2,ft9    # save f10 to Val2
+	fmv.s   fa0,fa2
+	
 	li	a7,4			#system call for print string
 	la	a0,ktemp
 	ecall
-	
-	fmv.s   fa2,ft9    # save f10 to Val2
-	#fmv.x.s	t2, ft9		# copy FP value to t reg
-	#fsw	fa2, 0(a0)	#store FP val to memory
 	li	a7,2			#system call for printing float fa0 in ascii
+	la	a0,ktemp
 	ecall
 	
 	j END
@@ -47,8 +48,8 @@ converter:
 	flw	ft3,num2,t0
 	flw	ft4,num3,t0
 	
-	fdiv.s	ft2,ft2,ft3 	#t4 = 5/9
-	fsub.s	ft4,ft1,ft4	#t6=ftemp - 32
+	fdiv.s	ft2,ft2,ft3 	#ft2 = 5/9
+	fsub.s	ft4,ft1,ft4	#ft4=ftemp - 32
 	fmul.s	ft6,ft2,ft4	#a1 = ctemp
 	
 	flw    	ft8,convert,t0     # ft4 = 273.15	
