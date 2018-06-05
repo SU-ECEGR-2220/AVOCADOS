41478bf0644e346d5af279e1bf9ed60d13d25e7d
ledonthuan <led7@seattleu.edu>
2018-06-04 19:49:51 -0700

    update

--- Lab 5/Memory.vhd
+++ Lab 5/Memory.vhd
@@ -1,8 +1,9 @@
-
 --------------------------------------------------------------------------------
 --
 -- LAB #5 - Memory and Register Bank
 --
+--------------------------------------------------------------------------------
+
 Library ieee;
 Use ieee.std_logic_1164.all;
 Use ieee.numeric_std.all;
@@ -28,7 +29,8 @@ begin
 	-- Note that data is output only when enout = 0	
 	bitout <= q when enout = '0' else 'Z';
 end architecture memlike;
----------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
 Library ieee;
 Use ieee.std_logic_1164.all;
 Use ieee.numeric_std.all;
@@ -44,9 +46,9 @@ end entity register8;
 architecture memmy of register8 is
 	component bitstorage
 		port(bitin: in std_logic;
-		 	 enout: in std_logic;
-		 	 writein: in std_logic;
-		 	 bitout: out std_logic);
+		     enout: in std_logic;
+		     writein: in std_logic;
+		     bitout: out std_logic);
 	end component;
 begin
 	-- insert your code here.
@@ -58,6 +60,7 @@ begin
 	b5: bitstorage port map (datain(5), enout, writein, dataout(5));
 	b6: bitstorage port map (datain(6), enout, writein, dataout(6));
 	b7: bitstorage port map (datain(7), enout, writein, dataout(7));
+
 end architecture memmy;
 
 --------------------------------------------------------------------------------
@@ -81,30 +84,26 @@ architecture biggermem of register32 is
 		     dataout: out std_logic_vector(7 downto 0));
 	end component;
 
-	signal enout_8: std_logic;
-	signal enout_16: std_logic;
-	signal writein_8: std_logic;
-	signal writein_16: std_logic;
-
+	signal enout_1: std_logic;
+	signal enout_2: std_logic;
+	signal writein_1: std_logic;
+	signal writein_2: std_logic;
 begin
 	-- insert code here.
-	-- when any of them are active low
-	enout_8 <= enout32 and enout16 and enout8;
-	-- when any of them are active high
-	writein_8 <= writein32 or writein16 or writein8;
+	
+	enout_1 <= enout32 and enout16 and enout8;
+	writein_1 <= writein32 or writein16 or writein8;
 
-	-- when either of them are active low
-	enout_16 <= enout32 and enout16;
-	-- when either of them are active high
-	writein_16 <= writein32 or writein16;
+	enout_2 <= enout32 and enout16;
+	writein_2 <= writein32 or writein16;
 
 
-	register0: register8 port map (datain(7 downto 0), enout_8, writein_8, dataout(7 downto 0));
-	register1: register8 port map (datain(15 downto 8), enout_16, writein_16, dataout(15 downto 8));
+	register0: register8 port map (datain(7 downto 0), enout_1, writein_1, dataout(7 downto 0));
+	register1: register8 port map (datain(15 downto 8), enout_2, writein_2, dataout(15 downto 8));
 	register2: register8 port map (datain(23 downto 16), enout32, writein32, dataout(23 downto 16));
 	register3: register8 port map (datain(31 downto 24), enout32, writein32, dataout(31 downto 24));
-end architecture biggermem;
 
+end architecture biggermem;
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 Use ieee.std_logic_1164.all;
@@ -133,18 +132,30 @@ begin
   begin
     if Reset = '1' then
       for i in 0 to 127 loop   
-          i_ram(i) <= X"00000000";
+          i_ram(i) <= "00000000000000000000000000000000";
       end loop;
     end if;
 
     if falling_edge(Clock) then
 	-- Add code to write data to RAM
 	-- Use to_integer(unsigned(Address)) to index the i_ram array
-	
+	if (WE = '1') then
+		if (to_integer(unsigned(Address)) < 128) then
+			i_ram(to_integer(unsigned(Address))) <= DataIn;
+		end if;
+	end if;	
     end if;
 
 	-- Rest of the RAM implementation
-
+	if rising_edge(Clock) then
+	if (OE = '0') then
+		if (to_integer(unsigned(Address)) < 128) then
+			DataOut <= i_ram(to_integer(unsigned(Address)));
+		else
+			DataOut <= "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"; 
+		end if;
+	end if;
+	end if;
   end process RamProc;
 
 end staticRAM;	
@@ -173,13 +184,12 @@ architecture remember of Registers is
 		 writein32, writein16, writein8: in std_logic;
 		 dataout: out std_logic_vector(31 downto 0));
 	end component;
-	
-	signal A0, A1, A2, A3, A4, A5, A6, A7, X0: std_logic (31 downto 0);
-	signal WRITETHIS: std_logic (7 downto 0);
-	
+
+	signal A0, A1, A2, A3, A4, A5, A6, A7, X0: std_logic_vector (31 downto 0);
+	signal WRITETHIS: std_logic_vector (7 downto 0);
 begin
     -- Add your code here for the Register Bank implementation
-	WITH WriteCmd & WriteReg SELECT WRITETHIS <= "00000001" when "101010",
+WITH WriteCmd & WriteReg SELECT WRITETHIS <= "00000001" when "101010",
 						"00000010" when "101011",
 						"00000100" when "101100",
 						"00001000" when "101101",
@@ -191,16 +201,16 @@ begin
 
 	X0 <= (others => '0');
 
- 	ALMOST: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(0), '1', '1', A0); 
-	DONE: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(1), '1', '1', A1); 
-	WTIH_: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(2), '1', '1', A2); 
-	THIS: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(3), '1', '1', A3); 
-	LAB: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(4), '1', '1', A4); 
-	IM: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(5), '1', '1', A5); 
-	SO: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(6), '1', '1', A6); 
-	HAPPY: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(7), '1', '1', A7); 
+ 	ALMOST: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(0), '0', '0', A0); 
+	DONE: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(1), '0', '0', A1); 
+	WTIH1: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(2), '0', '0', A2); 
+	THIS: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(3), '0', '0', A3); 
+	LAB: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(4), '0', '0', A4); 
+	IM: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(5), '0', '0', A5); 
+	SO: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(6), '0', '0', A6); 
+	HAPPY: register32 PORT MAP(WriteData, '0', '1', '1', WRITETHIS(7), '0', '0', A7); 
 
-	WITH ReadReg1 SELECT ReadData1 <= A0 when "01010"
+	WITH ReadReg1 SELECT ReadData1 <= A0 when "01010",
 					A1 when "01011",
 					A2 when "01100",
 					A3 when "01101",
@@ -210,7 +220,7 @@ begin
 					A7 when "10001",
 					X0 when others;
 
-	WITH ReadReg1 SELECT ReadData2 <= A0 when "01010"
+	WITH ReadReg1 SELECT ReadData2 <= A0 when "01010",
 					A1 when "01011",
 					A2 when "01100",
 					A3 when "01101",
@@ -221,9 +231,4 @@ begin
 					X0 when others;
 
 					
-
-	
-	
 end remember;
-
-----------------------------------------------------------------------------------------------------------------------------------------------------------------
