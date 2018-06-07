ec242806cc8260c381bf22f0fe419e531fc05d76
ledonthuan <led7@seattleu.edu>
2018-06-07 15:53:37 -0700

    update

--- Lab 6/ProcElements.vhd
+++ Lab 6/ProcElements.vhd
@@ -47,7 +47,7 @@ begin
 -- Add your code here
 	WITH opcode & funct3 SELECT Branch <= "01" when "1100011000", --BEQ
 						"10" when "1100011001", --BNE
-						"00" when others;
+						"--" when others;
 	
 	WITH opcode & funct3 SELECT MemRead <= '1' when "0000011010", --LW
 						'0' when others;
@@ -74,14 +74,30 @@ begin
 	WITH opcode & funct3 SELECT MemWrite <= '1' when "0100011010", -- SW
 						'0' when others;
 
-	WITH opcode & funct3 SELECT ALUSrc <= 
+	WITH opcode & funct3 & funct7 SELECT ALUSrc <= '0' when "01100110000000000", --ADD
+							'0' when "01100110001000000", --SUB
+							'0' when "01100111110000000", --AND
+							'0' when "01100111100000000", --OR
+							'0' when "1100011000-------", --BEQ
+							'0' when "1100011001-------", --BNE
+							'1' when others;
 
 	WITH opcode & funct3 SELECT RegWrite <= '0' when "0100011010", -- SW
 						'0' when "1100011000", -- BEQ
 						'0' when "1100011001", --BNE
 						'1' when others;
 
-	WITH opcode & funct3 & funct7 SELECT ImmGen <= "	
+	WITH opcode & funct3 SELECT ImmGen <= "00" when "0010011001", --SLLI
+						"00" when "0010011101", --SRLI
+						"00" when "0000011010", --LW
+						"00" when "0010011000", --ADDI
+						"00" when "0010011110", --ORI
+						"00" when "0010011111", --ANDI
+						"01" when "0100011010", --SW
+						"10" when "1100011000", --BEQ
+						"10" when "1100011001", --BNE
+						"11" when "0110111---", --LUI
+						"--" when others;
 end Boss;
 
 --------------------------------------------------------------------------------
@@ -100,6 +116,13 @@ end entity ProgramCounter;
 architecture executive of ProgramCounter is
 begin
 -- Add your code here
-	
+	ProgramCounter: Process(Reset, Clock)
+	begin
+		if reset = '1' then
+			PCout <= X"00400000";
+		elsif (rising_edge(clock)) then
+			PCout <= PCin;
+		end if;
+	end process;
 end executive;
 --------------------------------------------------------------------------------
