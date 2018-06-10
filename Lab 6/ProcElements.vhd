51e578a68145664085d7042729f41959cfc38fea
Thanh Nguyen <nguye408@seattleu.edu>
2018-06-10 13:18:03 -0700

    update

--- Lab 6/ProcElements.vhd
+++ Lab 6/ProcElements.vhd
@@ -1,4 +1,4 @@
- --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 --
 -- LAB #6 - Processor Elements
 --
@@ -16,9 +16,9 @@ end entity BusMux2to1;
 
 architecture selection of BusMux2to1 is
 begin
-	with selector select
-		Result <= In0 when '0',
-			In1 when others;
+-- Add your code here
+	WITH selector SELECT Result <= In0 when '0',
+					In1 when OTHERS;
 end architecture selection;
 
 --------------------------------------------------------------------------------
@@ -43,39 +43,62 @@ entity Control is
 end Control;
 
 architecture Boss of Control is
-
-signal result: std_logic_vector(13 downto 0);
-
 begin
 -- Add your code here
-	with funct7 & funct3 & opcode select
-		result <= "--0000000001--" when "00000000000110011", --ADD
-			 "--0000100001--" when "01000000000110011", --SUB
-			 "--0000010001--" when "00000001110110011", --AND
-			 "--0000011001--" when "00000001100110011", --OR
-			 "--0000001011--" when "00000000010110011", --SLL
-			 "--0001001011--" when "00000001010110011", --SLR
-			 "--000000001101" when "-------0000010011", --ADDI
-			 "--000001101101" when "00000001100010011", --ORI
-			 "--000001001101" when "00000001110010011", --ANDI
-			 "--110111001101" when "00000000100000011", --LW
-			 "--001010111010" when "-------0100100011", --SW
-			 "01001000000000" when "-------0001100011", --BEQ
-			 "10000100000000" when "-------0011100011", --BNE
-			 "--000111101111" when "----------0110111", --LUI
-			 "11111111111111" when others;				
+	WITH opcode & funct3 SELECT Branch <= "01" when "1100011000", --BEQ
+						"10" when "1100011001", --BNE
+						"--" when others;
+	
+	WITH opcode & funct3 SELECT MemRead <= '1' when "0000011010", --LW
+						'0' when others;
+	
+	WITH opcode & funct3 SELECT MemtoReg <= '1' when "0000011010", --LW
+						'0' when others;
 
-	Branch <= result(13 downto 12);
-	MemRead <= result(11);
-	MemtoReg <= result(10);
-	ALUCtrl <= result(9 downto 5);
-	MemWrite <= result(4);
-	ALUSrc <= result(3);
-	RegWrite <= result(2);
-	ImmGen <= result(1 downto 0);
+	WITH opcode & funct3 & funct7 SELECT ALUCtrl <= "0-000" when "01100110000000000", -- ADD
+							"0-100" when "01100110000100000", -- SUB
+							"0--10" when "01100111110000000", --AND
+							"0--11" when "01100111100000000", -- OR
+							"00-01" when "00100110010000000", --SLLI
+							"01-01" when "00100111010000000", --SRLI
+							"01-10" when "0000011010-------", --LW
+							"0-000" when "0010011000-------", --ADDI
+							"0--11" when "0010011110-------", --ORI
+							"0--10" when "0010011111-------", --ANDI
+							"10-01" when "0100011010-------", --SW
+							"1--00" when "1100011000-------", --BEQ
+							"0--00" when "1100011001-------", --BNE
+							"01-11" when "0110111----------", --LUI
+							"11111" when others;
+	
+	WITH opcode & funct3 SELECT MemWrite <= '1' when "0100011010", -- SW
+						'0' when others;
 
+	WITH opcode & funct3 & funct7 SELECT ALUSrc <= '0' when "01100110000000000", --ADD
+							'0' when "01100110001000000", --SUB
+							'0' when "01100111110000000", --AND
+							'0' when "01100111100000000", --OR
+							'0' when "1100011000-------", --BEQ
+							'0' when "1100011001-------", --BNE
+							'1' when others;
 
-end architecture Boss;
+	WITH opcode & funct3 SELECT RegWrite <= '0' when "0100011010", -- SW
+						'0' when "1100011000", -- BEQ
+						'0' when "1100011001", --BNE
+						'1' when others;
+
+	WITH opcode & funct3 SELECT ImmGen <= "00" when "0010011001", --SLLI
+						"00" when "0010011101", --SRLI
+						"00" when "0000011010", --LW
+						"00" when "0010011000", --ADDI
+						"00" when "0010011110", --ORI
+						"00" when "0010011111", --ANDI
+						"01" when "0100011010", --SW
+						"10" when "1100011000", --BEQ
+						"10" when "1100011001", --BNE
+						"11" when "0110111---", --LUI
+						"--" when others;
+end Boss;
 
 --------------------------------------------------------------------------------
 library IEEE;
@@ -92,12 +115,11 @@ end entity ProgramCounter;
 
 architecture executive of ProgramCounter is
 
-signal new_address: std_logic_vector (31 downto 0);
-
+	signal new_address: std_logic_vector(31 downto 0);
 begin
 -- Add your code here
-process(Clock, Reset)
-	begin
+ProgramCounter: Process(Reset, Clock)
+begin
 	if (Reset = '1') then
 		new_address <= "00000000010000000000000000000000";
 	end if;
@@ -107,6 +129,6 @@ process(Clock, Reset)
 	if (rising_edge(Clock)) then
 		PCOut <= new_address;
 	end if;
-end process;
+end process;	
 end executive;
 --------------------------------------------------------------------------------
