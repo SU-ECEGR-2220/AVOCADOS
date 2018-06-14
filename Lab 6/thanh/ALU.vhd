ad6f0d2fc1ac26ced8ea4301e9cdbb67ee685a9c
ledonthuan <led7@seattleu.edu>
2018-06-14 12:39:21 -0700

    FINAL LAB DONE

---
+++ Lab 6/thanh/ALU.vhd
@@ -0,0 +1,68 @@
+--------------------------------------------------------------------------------
+--
+-- LAB #4
+--
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
+
+entity ALU is
+	Port(	DataIn1: in std_logic_vector(31 downto 0);
+		DataIn2: in std_logic_vector(31 downto 0);
+		ALUCtrl: in std_logic_vector(4 downto 0);
+		Zero: out std_logic;
+		ALUResult: out std_logic_vector(31 downto 0) );
+end entity ALU;
+
+architecture ALU_Arch of ALU is
+	signal addsub_result: std_logic_vector(31 downto 0);
+	signal addsub_carryout: std_logic;
+	signal shift_result: std_logic_vector(31 downto 0);
+	signal and_result: std_logic_vector(31 downto 0);
+	signal or_result: std_logic_vector(31 downto 0);
+	signal final_result: std_logic_vector(31 downto 0);
+
+	-- ALU components	
+	component adder_subtracter
+		port(	datain_a: in std_logic_vector(31 downto 0);
+			datain_b: in std_logic_vector(31 downto 0);
+			add_sub: in std_logic;
+			dataout: out std_logic_vector(31 downto 0);
+			co: out std_logic);
+	end component adder_subtracter;
+
+	component shift_register
+		port(	datain: in std_logic_vector(31 downto 0);
+		   	dir: in std_logic;
+			shamt:	in std_logic_vector(4 downto 0);
+			dataout: out std_logic_vector(31 downto 0));
+	end component shift_register;
+
+begin
+	-- Add ALU VHDL implementation here
+	addsub: adder_subtracter port map(DataIn1, DataIn2, ALUCtrl(2), addsub_result, addsub_carryout);
+	shift: shift_register port map(DataIn1, ALUCtrl(3), DataIn2(4 downto 0), shift_result);
+	
+	and_result <= DataIn1 and DataIn2;
+	or_result <= DataIn1 or DataIn2;
+	
+	with ALUCtrl( 1 downto 0) select
+		final_result <= addsub_result when "00",
+				shift_result when "01",
+				and_result when "10",
+				or_result when "11",
+				DataIn2 when others;
+
+	with final_result select
+		Zero <= '1' when  "00000000000000000000000000000000",
+			'0' when others;
+
+	ALUResult <= final_result;
+
+end architecture ALU_Arch;
+
+
---
+++ Lab 6/thanh/ProcElements.vhd
@@ -0,0 +1,106 @@
+--------------------------------------------------------------------------------
+--
+-- LAB #6 - Processor Elements
+--
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity BusMux2to1 is
+	Port(	selector: in std_logic;
+			In0, In1: in std_logic_vector(31 downto 0);
+			Result: out std_logic_vector(31 downto 0) );
+end entity BusMux2to1;
+
+architecture selection of BusMux2to1 is
+begin
+-- Add your code here
+	WITH selector SELECT Result <= In0 when '0',
+					In1 when OTHERS;
+end architecture selection;
+
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity Control is
+      Port(clk : in  STD_LOGIC;
+           opcode : in  STD_LOGIC_VECTOR (6 downto 0);
+           funct3  : in  STD_LOGIC_VECTOR (2 downto 0);
+           funct7  : in  STD_LOGIC_VECTOR (6 downto 0);
+           Branch : out  STD_LOGIC_VECTOR(1 downto 0);
+           MemRead : out  STD_LOGIC;
+           MemtoReg : out  STD_LOGIC;
+           ALUCtrl : out  STD_LOGIC_VECTOR(4 downto 0);
+           MemWrite : out  STD_LOGIC;
+           ALUSrc : out  STD_LOGIC;
+           RegWrite : out  STD_LOGIC;
+           ImmGen : out STD_LOGIC_VECTOR(1 downto 0));
+end Control;
+ 
+ architecture Boss of Control is
+
+signal result: std_logic_vector(13 downto 0);
+
+ begin
+ -- Add your code here
+		result <= "000000000001ZZ" when funct7 = "0000000" and funct3 = "000" and opcode = "0110011" else --ADD
+			 "000000100001ZZ" when funct7 = "0100000" and funct3 = "000" and opcode = "0110011" else --SUB
+			 "000000010001ZZ" when funct7 = "0000000" and funct3 = "111" and opcode = "0110011" else --AND
+			 "000000011001ZZ" when funct7 = "0000000" and funct3 = "110" and opcode = "0110011" else --OR
+			 "000000001011ZZ" when funct7 = "0000000" and funct3 = "001" and opcode = "0110011" else --SLL			 
+			 "000001001011ZZ" when funct7 = "0000000" and funct3 = "101" and opcode = "0110011" else --SLR
+			 "00000000101101" when funct7 = "0000000" and funct3 = "001" and opcode = "0010011" else --SLLI
+			 "00000100101101" when funct7 = "0000000" and funct3 = "101" and opcode = "0010011" else --SRLI
+			 "00000000001101" when funct3 = "000" and opcode = "0010011" else --ADDI
+			 "00000001101101" when funct3 = "110" and opcode = "0010011" else --ORI
+			 "00000001001101" when funct3 = "111" and opcode = "0010011" else --ANDI
+			 "00010110001101" when funct3 = "010" and opcode = "0000011" else --LW			 
+			 "00101010011010" when funct3 = "010" and opcode = "0100011" else --SW
+			 "01001000000000" when funct3 = "000" and opcode = "1100011" else --BEQ
+			 "10000100000000" when funct3 = "001" and opcode = "1100011" else --BNE
+			 "00000111101111" when opcode = "0110111" else --LUI
+			 "11111111111111";	
+
+
+	Branch <= result(13 downto 12);
+	MemRead <= result(11);
+	MemtoReg <= result(10);
+	ALUCtrl <= result(9 downto 5);
+	MemWrite <= result(4);	
+	ALUSrc <= result(3);
+	RegWrite <= not(clk) and result(2);
+	ImmGen <= result(1 downto 0);	
+
+end architecture Boss;
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity ProgramCounter is
+    Port(Reset: in std_logic;
+	 Clock: in std_logic;
+	 PCin: in std_logic_vector(31 downto 0);
+	 PCout: out std_logic_vector(31 downto 0));
+end entity ProgramCounter;
+
+architecture executive of ProgramCounter is
+
+begin
+-- Add your code here
+	Process(Reset,Clock)
+	begin	
+ 		if Reset = '1' then
+			PCout <= X"003FFFFC"; --reset to start at address 0x003FFFFC
+		elsif rising_edge(Clock) then --not sure if falling_edge or rising_edge
+			PCout <= PCin; --maintains the address of the next instruction
+		end if;
+	end process; 
+end executive;
+--------------------------------------------------------------------------------
